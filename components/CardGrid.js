e5bd9b1fd7dbfacd18fc5b54a7874c559a15015e
Howard Huang <howardhuang2004@gmail.com>
2023-04-22 12:40:10 -0700

    add hotel room cards

---
+++ components/CardGrid.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AirbnbCard from '@/components/AirbnbCard'
+
+function Cardgrid() {
+  return (
+    <div>
+      <AirbnbCard />
+      <AirbnbCard />
+      <AirbnbCard />
+    </div>
+  )
+}
+
+export default Cardgrid
