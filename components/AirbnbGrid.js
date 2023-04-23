ccb982ac30361e73ae11152650cd3f4722cec97c
Howard Huang <howardhuang2004@gmail.com>
2023-04-22 18:32:39 -0700

    add marketplace page

--- components/AirbnbGrid.js
+++ components/AirbnbGrid.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import AirbnbCard from '@/components/AirbnbCard'
 
-function CardGrid({roomInfo}) {
-  console.log("roomInfo in CardGrid:" , roomInfo)
-  console.log("roomInfo.message in CardGrid:" , roomInfo.message)
+function AirbnbGrid({roomInfo}) {
+  // console.log("roomInfo in CardGrid:" , roomInfo)
+  // console.log("roomInfo.message in CardGrid:" , roomInfo.message)
   const items = roomInfo?.message?.results;
-  console.log("items:" , items);
+  // console.log("items:" , items);
   return (
     <div className="grid grid-cols-5 gap-5">
       {items && items.map((item) => {
@@ -15,4 +15,4 @@ function CardGrid({roomInfo}) {
   )
 }
 
-export default CardGrid
+export default AirbnbGrid
