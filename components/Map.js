2f75339fab0478ed4ac8cfcd5e359fde732a3719
joe-lin-tech <lin0joe24@gmail.com>
2023-04-23 00:20:15 -0700

    Update Map + API Requests

--- components/Map.js
+++ components/Map.js
@@ -1,9 +1,8 @@
 import { useEffect, useRef, useState } from "react"
 
-const Map = ({ location, zoomLevel, places }) => {
+const Map = ({ location, zoomLevel, markers }) => {
   const ref = useRef();
   const [map, setMap] = useState();
-  // const [markers, setMarkers] = useState([]);
 
   useEffect(() => {
     if (ref.current && !map) {
@@ -14,15 +13,34 @@ const Map = ({ location, zoomLevel, places }) => {
   useEffect(() => {
     // for (let i = 0; i < markers.length; i++) markers[i].setMap(null)
     // setMarkers([]);
-    places.map((p) => {
-      new google.maps.Marker({
-        position: { lat: p.coordinates.latitude, lng: p.coordinates.longitude },
-        map,
-        // title: "Hello World!",
-        // icon: "/duck.webp"
-      })
-    })
-  }, [map, places])
+    // const directions = new google.maps.DirectionsService();
+    // if (places.length > 1) {
+    //   directions.route({
+    //     origin: { lat: places[0].coordinates.latitude, lng: places[0].coordinates.longitude },
+    //     destination: { lat: places[places.length - 1].coordinates.latitude, lng: places[places.length - 1].coordinates.longitude },
+    //     travelMode: "DRIVING"
+    //   }, (res, status) => {
+    //     if (status == "OK") {
+    //       new google.maps.DirectionsRenderer({
+    //         suppressMarkers: true,
+    //         directions: res,
+    //         map: map
+    //       })
+    //     }
+    //   })
+    // }
+    // places.map((p) => {
+    //   new google.maps.Marker({
+    //     position: { lat: p.coordinates.latitude, lng: p.coordinates.longitude },
+    //     map,
+    //     // title: "Hello World!",
+    //     // icon: "/duck.webp"
+    //   })
+    // })
+    console.log("MARKERS IN MAP", markers)
+    for (let i = 0; i < markers.length; i++) markers[i].setMap(map)
+    console.log("MAP EFFECT")
+  }, [map, markers])
 
   return (
     <div className="h-full" ref={ref} />
